f59d695788da023a2de5fe7cf0a69db6feb9a25e
fur-lab <m.rafli.a.risdi.s@gmail.com>
2025-09-25 13:59:05 +0800

    modul 3

---
+++ kelas/pertemuan-3/mainmod3.py
@@ -0,0 +1,65 @@
+# nilai = 70
+
+# if nilai > 75:
+#     print("Nilai A")
+# elif nilai > 65:
+#     print("Nilai B")
+# else:
+#     print("Nilai c")
+
+# suhu = 35
+# if suhu >= 35:
+#     print("Panas")
+# if suhu > 30:
+#     print("Sauna")
+# elif suhu > 20:
+#     print("Normal")
+# else:
+#     print("Dingin")
+
+#
+# umur = int(input("Masukkan umur: "))
+# if umur < 13:
+#     kategori = "Anak-anak"
+# elif umur < 18:
+#     kategori = "Remaja"
+# elif umur < 60:
+#     kategori = "Dewasa"
+# else:
+#     kategori = "Lansia"
+# print("Umur: ", umur, "Kategori :", kategori)
+
+# Ternary Operator
+# nilai = 70
+# status = "Lulus" if nilai >= 60 else "Tidak Lulus"
+# print(status)
+
+# studi kasus 1
+# tinggi =int(input("Masukkan tinggi badan (cm): "))
+# bisakah =  "Silahkan masuk wahana" if tinggi >= 160 else "Tidak boleh masuk wahana"
+# print(bisakah)
+
+
+# program kalukaltor volume bangun ruang 
+# print("Pilih bangun ruang: ")
+# print("1. Kubus")
+# print("2. Balok")
+# print("3. Tabung")
+
+# pilihan = input("Masukkan pilihan (1/2/3): ")
+# if pilihan == "1":
+#     sisi = float(input("Masukkan panjang sisi kubus (cm): "))
+#     volume = sisi ** 3
+#     print("Volume kubus adalah:", volume, "cm³")
+# elif pilihan == "2":
+#     panjang = float(input("Masukan panjang (cm): "))
+#     lebar = float(input("Masukan lebar (cm): "))
+#     tinggi = float(input("Masukan tinggi (cm): "))
+#     volumebalok = panjang*lebar*tinggi
+#     print("Volume Balok adalah : ", volumebalok)
+# elif pilihan == "3":
+#     phi = 3,14
+#     jarijari = float(input("Masukan jari-jari (cm): "))
+#     tinggi2 = float(input("Masukan tinggi (cm): "))
+#     volumetabung = phi*(jarijari**2)*tinggi2
+#     print("Volume tabung adalah :", volumetabung)
