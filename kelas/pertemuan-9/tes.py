c83d578153620b74ec95da1434139df0fb6a77df
M.R ars <m.rafli.a.risdi.s@gmail.com>
2025-11-06 09:39:51 +0800

    ya

---
+++ kelas/pertemuan-9/tes.py
@@ -0,0 +1,31 @@
+# # Matplotlib
+
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import numpy as np
+
+# # Inisialisasi kanvas = buat uk tabelnya
+# plt.figure(figsize=(12, 6))
+
+# # info tambahan pada kanvas
+# plt.xlabel('kasih nama Sumbu X')
+# plt.ylabel('kasih nama Sumbu Y')
+# plt.title("Ini judul loh") # ini buat judulnya
+
+# x = [1, 2, 3, 4, 5]
+# y = [10, 35, 20, 70, 60]
+
+
+# ####### beda visual ###########
+
+# plt.figure(figsize=(8, 4))
+# # plt.plot()
+# plt.plot(x, y)
+# # Detail element
+# plt.title("Contoh Grafik")
+# plt.xlabel("Sumbu X")
+# plt.ylabel("Sumbu Y")
+# plt.grid(True)
+# plt.show() # Ini buat  menampilkannya, jika tidak ada maka tidak ada visualisasi
+
+# sisanya bisa lihat modul dan gogcollab
